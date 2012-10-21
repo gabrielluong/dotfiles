136235828fbbac698b4d34d327b05d3241955b3a
Gabriel Luong <gabriel.luong@gmail.com>
2012-10-21 16:01:42 -0400

    Added comments to setup_symlinks

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+# Enable Sublime Text 2 as a command line tool using subl
+ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl /usr/local/bin/Sublime
