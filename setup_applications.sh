30623064bd499daf053d8f444b2ad894c459bb7f
Gabriel Luong <gabriel.luong@gmail.com>
2012-12-12 20:54:09 -0500

    Added brew cask to install applications

---
+++ setup_applications.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Get brew-cask
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+brew cask install adium
+brew cask install alfred
+brew cask install dropbox
+brew cask install firefox
+brew cask install flux
+brew cask install google-chrome
+brew cask install google-chrome-canary
+brew cask install istat-menus
+brew cask install iterm2
+brew cask install notational-velocity
+# brew cask install nv-alt
+brew cask install one-password
+brew cask install sequel-pro
+brew cask install skype
+brew cask install sublime-text
+brew cask install textwrangler
+brew cask install the-unarchiver
+brew cask install transmission
+
+brew cask linkapps
