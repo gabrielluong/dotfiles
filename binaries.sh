eaf5640e030b7181ea7316a4bd9cab420a7b514b
Gabriel Luong <gabriel.luong@gmail.com>
2014-10-28 15:39:20 -0400

    Updated packages.sh to binaries.sh

---
+++ binaries.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Binary installer
+# Based on Matthew Mueller's dots
+# https://github.com/MatthewMueller/dots/blob/master/os/osx/binaries.sh
+#
+
+# Check for Homebrew
+if test ! $(which brew); then
+	echo "Installing homebrew..."
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew
+brew update && brew cask update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install Bash 4
+brew install bash
+
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+
+# Install other useful binaries
+binaries=(
+	git
+	node
+	python
+	# rbenv
+	# ruby-build
+	wget
+)
+
+# Install the binaries
+brew install ${binaries[@]}
+
+# Remove outdated Homebrew packages
+brew cleanup
+
+exit 0
