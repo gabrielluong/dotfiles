37b1fd7fa05c74ac0a15e8d53da60ecb8cc45cb6
Gabriel Luong <gabriel.luong@gmail.com>
2018-09-01 09:40:35 -0400

    Update alias.zsh

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -6,14 +6,9 @@ alias ....='cd ../../..'
 # Shortcut to Projects
 p() { cd $HOME/Projects/$*; }
 
-# Shortcut to Courses
-c() { cd $HOME/Dropbox/Courses/$*; }
-
 # Shortcut to fx-team
 fx() { cd $HOME/Projects/fx-team/$*; }
 fxp() { cd $HOME/Projects/fx-team/.hg/patches/$*; }
-tf() { cd $HOME/Projects/tofino/$*; }
-
 hpfx() { hg qfinish -a; hg push inbound; }
 gpa() { fxp; ga .; gc -m "backup"; gp; fx; }
 
@@ -28,6 +23,7 @@ alias rf='rm -rf'
 # Firefox Development Aliases
 alias build='fx && ./mach build'
 alias bf='fx && ./mach build faster'
+alias br='fx && ./mach build faster && ./mach run'
 alias bd='fx && ./mach build devtools'
 alias run='fx && ./mach run'
 alias dt='fx && ./mach test'
@@ -35,11 +31,13 @@ alias mt='fx && ./mach mochitest'
 alias mtd='fx && ./mach mochitest --jsdebugger'
 alias try='fx && hp try && hg push -f try'
 alias purge='fx && hg revert -a && hg purge'
+alias review='hg push review'
 
 # Git Aliases
 alias gs='git status'
 alias gpom='git pull origin master'
 alias gpp='git pull --rebase upstream master && git push'
+alias gr='git remote update'
 
 # Mercurial Aliases
 alias hs='hg status'
